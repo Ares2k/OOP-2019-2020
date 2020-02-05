cddfdeaeda07ba29fa694191b4591785b71106a6
Timur <Timursultanov00@gmail.com>
2020-02-05 21:19:49 +0000

    Fixed movement and changed player model

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -4,12 +4,26 @@ import processing.core.PApplet;
 
 class BugZap extends PApplet {
 
-    private float playerX = 250;
-    private float playerY = 480;
+    //starting positions for the triangle player
+    private float playerX1 = 350;
+    private float playerY1 = 650;
+
+    private float playerX2 = 370;
+    private float playerY2 = 650;
+
+    private float playerX3 = 360;
+    private float playerY3 = 660;
+
     private float playerWidth = 50;
 
+    private float laserX1 = 0;
+    private float laserY1 = 0;
+
+    private float laserX2 = 0;
+    private float laserY2 = 0;
+
     public void settings() {
-        size(500, 500);
+        size(700, 700);
     }
 
     public void setup() {
@@ -17,17 +31,16 @@ class BugZap extends PApplet {
     }
 
     public void draw() {
-        background(0, 255, 0);
-        drawPlayer(playerX, playerY, playerWidth);
+        background(0, 0, 0);
+        drawPlayer(playerX1, playerY1, playerX2, playerY2, playerX3, playerY3, playerWidth);
+        drawLaser(laserX1, laserY1, laserX2, laserY2);
     }
 
-    public void drawPlayer(float x, float y, float w) {
-
-        float h = w/2;
-        w = w/2;
+    public void drawPlayer(float x1, float y1, float x2, float y2, float x3, float y3, float w) {
 
         fill(0, 0, 0);
-        ellipse(x, y, w, h);
+        stroke(255, 255, 255);
+        triangle(x1, y1, x2, y2, x3, y3);
     }
 
     public void keyPressed()
@@ -35,16 +48,51 @@ class BugZap extends PApplet {
         if (keyCode == LEFT)
         {
             System.out.println("Left arrow pressed");
-            playerX -= 6;
+
+            if (playerX1 < 10) {
+                playerX1 = 10;
+                playerX2 = 30;
+                playerX3 = 20;
+
+            } else {
+                playerX1 -= 6;
+                playerX2 -= 6;
+                playerX3 -= 6;
+            }
         }
+
         if (keyCode == RIGHT)
         {
             System.out.println("Right arrow pressed");
-            playerX = playerX + 6;
+
+            if (playerX2 > 690) {
+                playerX1 = 670;
+                playerX2 = 690;
+                playerX3 = 680;
+
+            } else {
+                playerX1 += 6;
+                playerX2 += 6;
+                playerX3 += 6;
+            }
         }
+
         if (key == ' ')
         {
             System.out.println("SPACE key pressed");
+
+            laserX1 = playerX1 + 10;
+            laserX2 = laserX1;
+
+            laserY1 = playerY1;
+            laserY2 = laserY1 - 700;
+
         }
+    }
+
+    public void drawLaser(float x1, float y1, float x2, float y2) {
+
+        stroke(0, 255, 0);
+        line(x1, y1, x2, y2);
     }
 }
